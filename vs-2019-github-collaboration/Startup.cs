c1f29e8d797bd548a420a2af31374639e4ec0d0d
jewillia33 <jewillia33@gmail.com>
2021-12-01 15:01:34 -0500

    Add another line ot the header

--- vs-2019-github-collaboration/Startup.cs
+++ vs-2019-github-collaboration/Startup.cs
@@ -33,6 +33,7 @@ namespace vs_2019_github_collaboration
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("<h1>Hello World!</h1>");
+                    await context.Response.WriteAsync("Welcome to my site!</br>");
                     await context.Response.WriteAsync("Welcome to my site!");
                 });
             });
